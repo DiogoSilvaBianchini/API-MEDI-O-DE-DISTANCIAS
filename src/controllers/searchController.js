e78808c965a0b97c350f285a7c5e81970830166e
unknown <diogosilvabianchini@gmail.com>
2024-11-01 15:11:44 -0300

    Melhoria na respota da API.

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -1,17 +1,16 @@
-const axios = require("axios")
 const getDistance = require("../utils/getDistance")
 const getCorde = require("../utils/getCorde")
 
 const getCord = async (req, res, next) => {
     const { street, city, state } = req.body
-    const {longitude, latitude} = await getCorde(street, city, state )
-    next({longitude, latitude})
+    const {longitude, latitude, address} = await getCorde(street, city, state )
+    next({longitude, latitude, address})
 }
 
 const getDeliveryDistance = async (corde, req, res, next) => {    
     try {
         const {km, minutes} = await getDistance(corde.longitude, corde.latitude)
-        return res.status(200).json({msg: "Percurso encontrado com sucesso!", results: {distance: km, duration: minutes}, status: 200})
+        return res.status(200).json({msg: "Percurso encontrado com sucesso!", results: {distance: km, duration: minutes, address: corde.address}, status: 200})
     } catch (error) {
         return res.status(500).json({msg: "Algo deu errado!", results: error, status: 500})
     }
@@ -19,15 +18,15 @@ const getDeliveryDistance = async (corde, req, res, next) => {
 
 const calculateShipping = async (corde, req, res, next) => {
     const {km} = await getDistance(corde.longitude, corde.latitude)
-    
     const shippingCostPerKm = 1.50
 
     const deliveryCost = (Math.floor(Number(km)) * shippingCostPerKm).toFixed(2)
 
     return res.status(200).json({msg: "Calculo gerado com sucesso!", results: {
-        price: {
-            deliveryCost,
-            distance: km
+        price: deliveryCost,
+        info: {
+            distance: km.toFixed(2),
+            address: corde.address
         }
     }, status: 200})
 }
--- src/utils/getCorde.js
+++ src/utils/getCorde.js
@@ -2,8 +2,9 @@ const axios = require("axios")
 
 const getCorde = async (street, city, state) => {
     const request = await axios.get(`https://api.mapbox.com/search/geocode/v6/forward?q=${street},${city}-${state}&country=BR&access_token=${process.env.MAP_BOX_SCRET_KEY}`)
+    const address = request.data.features[0].properties.full_address
     const {longitude, latitude} = request.data.features[0].properties.coordinates
-    return {longitude, latitude}
+    return {longitude, latitude, address}
 }
 
 module.exports = getCorde
