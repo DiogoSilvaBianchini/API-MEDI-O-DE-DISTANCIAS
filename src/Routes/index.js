597b329b4fd7cd2da2cd75c188bf8c8d3044fcca
unknown <diogosilvabianchini@gmail.com>
2024-10-31 21:39:59 -0300

    Adição da função para calculo de distância com localização fixa.

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
-const { getCord } = require("../controllers/searchController")
+const { getCord, getDistance } = require("../controllers/searchController")
 const router = express.Router()
 
 router.get("/", (req,res) => res.status(200).json({msg: "OK", status: 200}))
-router.post("/search", express.json(), getCord)
+router.post("/search", express.json(), getCord, getDistance)
 module.exports = router
--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -8,10 +8,30 @@ const getCord = async (req,res,next) => {
         const search = response.data.features[0].properties
         const corde = search.coordinates
 
-        return res.status(200).json({msg: "Local encontrado", results: corde, state: 200})
+        next(corde)
     }).catch(error => {
-        return res.status(200).json({msg: "Algo deu errado", results: error, state: 200})
+        return res.status(500).json({msg: "Algo deu errado", results: error, state: 500})
     })
 }
 
-module.exports = {getCord}
+const getDistance = (corde,req,res,next) => {
+    console.log(corde.longitude, corde.latitude)
+    
+    axios.get(`https://api.mapbox.com/directions/v5/mapbox/driving/-47.440185%2C-23.506525%3B${corde.longitude}%2C${corde.latitude}?alternatives=true&geometries=geojson&language=pt&overview=full&steps=true&access_token=${process.env.MAP_BOX_SCRET_KEY}`)
+    .then(response => {
+        const {distance, duration} = response.data.routes[0]
+        
+        const km = (distance / 1000).toFixed(2)
+        const minutes = (duration / 60).toFixed(2)
+
+        return res.status(200).json({msg: "Caminho encontrado!", results: {distance: km, duration: minutes}, status: 200})
+    }).catch(error => {
+        return res.status(500).json({msg: "Algo deu errado", results: error, state: 500})
+    })
+}
+
+
+module.exports = {
+    getCord,
+    getDistance
+}
